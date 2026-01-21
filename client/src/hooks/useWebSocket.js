a698f79dccc555be4318b065ff6c993016cafe90
Singh Payal <118951240+payal-22@users.noreply.github.com>
2026-01-21 23:04:15 +0530

    added render link

--- client/src/hooks/useWebSocket.js
+++ client/src/hooks/useWebSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:5000';
+const SERVER_URL = 'https://flam-sde-assignment.onrender.com';
 
 export const useWebSocket = (roomId) => {
   const socketRef = useRef(null);
@@ -113,4 +113,5 @@ export const useWebSocket = (roomId) => {
     saveCanvasState,
     loadCanvasState
   };
+
 };
